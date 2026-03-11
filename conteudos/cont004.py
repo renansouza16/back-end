08e4fe62585e8ff686f7f71d822a8aa02080389b
renansouza16 <renan.souza11@escola.pr.gov.br>
2026-03-11 11:41:37 -0300

    Create cont004.py

---
+++ conteudos/cont004.py
@@ -0,0 +1,8 @@
+#OPERAORES ARITMÉTICOS
+# 0 + é utilizado como operador de adição
+# 0 - é utilizado como sinal de subtração
+# 0 * é utilizado para fazer a multiplicação 
+# / é utilizado para fazer divisão 
+# ** é utilizado para fazer a exponenciação 
+# // é utilizado para fazer a divisão inteira
+# % é utilizado para o resto da divisão
