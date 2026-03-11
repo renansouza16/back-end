f8a22d9b28374b7a3dce22f23d43c4363ae286b3
renansouza16 <renan.souza11@escola.pr.gov.br>
2026-03-11 11:32:23 -0300

    Create ex005.py

---
+++ exercício/ex005.py
@@ -0,0 +1,4 @@
+n = input('digite algo')
+print(n.isalpha())#para perguntar se é letra
+print(n.isnumeric())#para perguntar se é um número
+print(n.islower())#para verificar se é apenas letras minusculas
