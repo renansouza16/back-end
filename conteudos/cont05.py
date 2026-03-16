00fabf9d388b42804709b5aef7ceb6e19c0fdc1c
renansouza16 <renan.souza11@escola.pr.gov.br>
2026-03-16 10:54:50 -0300

    Create cont05.py

---
+++ conteudos/cont05.py
@@ -0,0 +1,16 @@
+##ORDEM DE PROCEDỄNCIA
+# Definição: é a ordem na qual operadores serão executados
+# 1 Expressões dentro de parenteses
+# 2 Em seguida vem as protenciações
+# 3 Em terceiro lugar vem as multiplicação, a divisão normal, a divisão inteira e o resto da divisão. No caso de ter mais um deles vocễ resolve quem aparecer primeiro
+# 4 E por último a soma e a subtração
+
+# 5+3*2==
+# 3*5+4**2=
+# 3*(5+4)**2=
+# O Python é utilizado em cálculos científicos é o tamanho da memoria ou seja conseguimos chegar númeors extremamentes grandes
+
+# Para exponenciação podemos usar o pow(x,y) onde o número x é elevado ao número y
+# Para raiz quadrada podemos usar o **(1/2), elevando o número a 1/2 nós obtemos a raiz quadrada, para raiz cúbica **(1/3), e assim por diante.
+# Podemos usar os operadores em strings
+# Ex '='*5 vai ficar '====='
