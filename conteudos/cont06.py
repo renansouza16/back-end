cfed51e03163c795995f9e2221bc97b11b9efbd8
renansouza16 <renan.souza11@escola.pr.gov.br>
2026-03-16 11:02:07 -0300

    Create cont06.py

---
+++ conteudos/cont06.py
@@ -0,0 +1,3 @@
+# Podemos escrever um resultado na quantidade de caracteres específica ao colocar o valor dentro da mácara após escrever
+nome = input ('Qual é o seu nome?')
+print('prazer em conhece-lo {:20}!'.format(nome))
