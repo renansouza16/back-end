c1306707ce585323d98c06cb0771b54521a42542
renansouza16 <renan.souza11@escola.pr.gov.br>
2026-03-26 08:38:11 -0300

    Create Biblioteca03.py

---
+++ conteudos/Biblioteca03.py
@@ -0,0 +1,7 @@
+A funcionalidade trunc, elimina números da vírgula pra frente.
+A funcionalidade pow é usada para a exponenciação, semelhante ao uso de 2 asteríscos.
+A funcionalidade sqrt utilizada para calcular a raiz quadrada
+A funcionalidade factorial calcula o fatorial de um número
+Ex: import math (importa tudo para o programa).
+From math import sqrt, ceil(importa apenas a funcionalidade sqrt e a ceil).
+A IA pode cometer erros. 
