15c2c83d5a4dfd2f7698029adde5ba98e3e31d04
renansouza16 <renan.souza11@escola.pr.gov.br>
2026-05-11 10:45:20 -0300

    Update ex027.py

--- exercício/ex027.py
+++ exercício/ex027.py
@@ -7,3 +7,10 @@ nomes = nome_completo.split()
 # Exibe o primeiro e o último nome
 print(f'primeiro: {nomes[0]}')
 print(f'último: {nomes[-1]}')
+_________________________________________________
+nome = str(input("Digite seu nome completo: ")).strip()
+dividido = nome.split()
+
+print("Muito prazer em te conhecer!")
+print("Seu primeiro nome é {}".format(dividido[0]))
+print("Seu ultimo nome é {}".format(dividido[len(dividido)-1]))
