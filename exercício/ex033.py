746cb4c427e5784e75645b43d60de11ea96f3ffd
renansouza16 <renan.souza11@escola.pr.gov.br>
2026-05-18 10:51:02 -0300

    Create ex033.py

---
+++ exercício/ex033.py
@@ -0,0 +1,20 @@
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+n3 = float(input("Digite o terceiro número: "))
+
+# Definindo o menor
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+
+# Definindo o maior
+maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+
+print("O maior valor digitado foi {}".format(maior))
+print("O menor valor foi {}".format(menor))
