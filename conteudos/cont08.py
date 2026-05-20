13998ab19e0279f3290d780cb8e3829165289e44
renansouza16 <renan.souza11@escola.pr.gov.br>
2026-05-20 10:55:35 -0300

    Create cont08.py

---
+++ conteudos/cont08.py
@@ -0,0 +1,6 @@
+# CONDIÇÕES ALINHADAS 
+# São condicões colocadas dentro de outras condições.Em conmdições alinhadas eu utilizo o se não se,
+# Dentro do se. Python o se não se é representado por elif condição:
+# A partir do primeiro elif para condições você vai colocando um elif para cada condição.
+# Sempre que for usar o primeiro elif  tem que usar um if antes e o else se torna opcional.
+
