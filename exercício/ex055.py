f540410dc96c6155be75ec554318bed64702513a
renansouza16 <renan.souza11@escola.pr.gov.br>
2026-08-20 11:18:27 -0300

    Update ex055.py

--- exercício/ex055.py
+++ exercício/ex055.py
@@ -1,6 +1,16 @@
-sexo = input('Informe seus dados [m/f]: ').strip().lower()[:1]
-while sexo not in ['m', 'f'] or not sexo:
-    print('Dados inconsistentes!')
-    sexo = input('Informe novamente [m/f]: ').strip().lower()[:1]
-
-print(f'O seu sexo é {sexo}')
+from random import randint
+print('Sou seu computador')
+print('''Acabei de pensar em um número entre 0 e 10.
+Será que você consegue adivinhar qual foi?''')
+palpite = int(input('Qual é o seu palpite? '))
+aleatorio = randint(0,10)
+tentativas = 0
+while aleatorio != palpite:
+    if aleatorio > palpite:
+        palpite= int(input('Mais... Tente mais uma vez'))
+        tentativas += 1
+    elif aleatorio < palpite:
+        palpite = int(input('Menos... Tente mais uma vez'))
+        tentativas += 1
+tentativas+= 1
+print(f'Acertou com {tentativas} tentativas. Parabéns')
