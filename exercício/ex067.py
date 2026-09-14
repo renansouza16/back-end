a2e126006e7e3c966257614918e31377f69a21bb
renansouza16 <renan.souza11@escola.pr.gov.br>
2026-09-14 11:31:53 -0300

    Create ex067.py

---
+++ exercício/ex067.py
@@ -0,0 +1,32 @@
+print('-'*15)
+print('loja super baratão')
+print('-'*15)
+total = totmil = menor = cont = 0
+barato = ' '
+while True:
+    produto = str(input('Nome do Produto: '))
+    preco = float(input('Preço: R$ '))
+    cont += 1
+    total += preco
+    if preco > 1000:
+        totmil += 1
+    if cont == 1:
+        menor = preco
+        barato = produto
+
+____________________________________________________________________________________________________________
+
+        else:
+            if preco < menor:
+                menor = preco
+                barato = produto
+        continuar = ' '
+        while continuar not in 'sn':
+            continuar = str(input('Quer continuar? [s/n] ')).strip().lower()[0]
+        if continuar in 'n':
+            break
+
+print('Fim do programa')
+print(f'O total da compra foi {total:.2f}')
+print(f'Temos {cont} produtos custando mais de R$1000.00')
+print(f'O produto mais barato foi {barato} e custa R${menor:.2f}')
